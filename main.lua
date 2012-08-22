4db38ef35d82dc3eb0bba2ad9969f614fc9c53fa
Eric Gaudet <gaudet.eric@gmail.com>
2012-08-21 20:12:27 -0700

    try to load the last saved during init

--- main.lua
+++ main.lua
@@ -1,5 +1,20 @@
 require "menu"
 require "eyes"
+require "libuserdata"
+
+local file_userdata = "userdata.tsv"
+
+userdata = nil
+
+local default_userdata = {
+  -- survival
+  best = 0,
+  survival = false,
+  -- adventure
+  chapter = 0,
+  balls = 0,
+  score = 0
+}
 
 local state = nil
 
@@ -9,9 +24,11 @@ function change_state(s)
   state.load()
 end
 
-function love.load()
-  math.randomseed(os.time())
+function save_userdata()
+  libuserdata.save(file_userdata, userdata)
+end
 
+function love.load()
   love.graphics.setBackgroundColor(0,0,0)
 
   default_font = love.graphics.newImageFont("assets/font.png",
@@ -22,6 +39,12 @@ function love.load()
   sound_no_long = love.audio.newSource("assets/no_long.mp3", "static")
   sound_lost = love.audio.newSource("assets/lost1.mp3", "static")
   sound_lost_all = love.audio.newSource("assets/lost_all.mp3", "static")
+
+  userdata = libuserdata.load(file_userdata)
+  if not userdata then
+    print "Using default userdata"
+    userdata = default_userdata
+  end
 
   init_eyes()
   change_state(menu)
@@ -54,12 +77,14 @@ function love.mousereleased(x,y,b)
 end
 
 function love.keypressed(key)
-  if (key == "escape") then
-    if (love.event.quit) then
+  if key == "escape" then
+    if love.event.quit then
       love.event.quit() -- 0.8
     else
       love.event.push("q") -- 0.7
     end
+  elseif key == "q" or key == "Q" then
+    change_state(menu)
   else
     state.keypressed(key)
   end
