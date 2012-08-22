2037350ecdca394bb41bc5f3cf3535d4ba75f796
Eric Gaudet <gaudet.eric@gmail.com>
2012-08-21 20:11:28 -0700

    a library to load and save a flat table

---
+++ libuserdata.lua
@@ -0,0 +1,45 @@
+-- Inspired by SICK
+
+libuserdata = {}
+
+function libuserdata.load(filename)
+  if not love.filesystem.exists(filename) then return false end
+  local file = love.filesystem.newFile(filename)
+  if not file:open("r") then return false end
+
+  local data = {}
+  for line in file:lines() do
+    local i = line:find('\t', 1, true)
+    local key = line:sub(1, i-1)
+    local value = line:sub(i+1)
+    if value == "false" then
+      value = false
+    elseif value == "true" then
+      value = true
+    else
+      value = tonumber(value) or value
+    end
+    print ("Load:", key, value)
+    data[key] = value
+  end
+  file:close()
+
+  return data
+end
+
+function libuserdata.save(filename, data)
+  local file = love.filesystem.newFile(filename)
+  if not file:open("w") then return false end
+
+  for k,v in pairs(data) do
+    print ("Save:", k, v)
+    file:write(k)
+    file:write("\t")
+    file:write(tostring(v))
+    file:write("\n")
+  end
+  file:close()
+
+  return true
+end
+
