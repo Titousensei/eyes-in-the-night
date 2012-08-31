4f996d0b316267033ba048f26ccfe62dd36ed664
Eric Gaudet <gaudet.eric@gmail.com>
2012-08-30 21:17:47 -0700

    the web page

---
+++ www/f_eyes-in-the-night.php
@@ -0,0 +1,76 @@
+<?php
+?>
+<style>
+* {
+  color: #475790;
+}
+</style>
+
+<ul>
+
+<b>Eyes in the Night</b> is a simple yet addictive multi-platform game
+with mechanics reminiscent of billiards, and a spooky theme.  It's a single
+player game, featuring a campaign with over 30 levels, and an unlockable
+Survival Challenge.
+<br><br>
+<table align="center"><tr>
+<td><img src="eyes-in-the-night/eyes-in-the-night.screenshot2.png" border=0></td>
+<td><img src="eyes-in-the-night/eyes-in-the-night.screenshot1.png" border=0></td>
+</tr></table>
+<br><br>
+
+<b><u>Download:</u></b><br>
+<table align="center" width="100%"><tr>
+<td width="50%" valign="top">
+<ul>
+  <li><a href="eyes-in-the-night/eyes_in_the_night_win_x86.zip">.zip for Windows (32 bits)</a></li>
+  <li><a href="eyes-in-the-night/eyes-in-the-night_OSX.zip">.zip for Mac OS X</a></li>
+  <li><a href="eyes-in-the-night/eyes-in-the-night.2012-08-26.love">.love for Linux</a>
+    <br>(you must install <a href="https://love2d.org/">L&Ouml;VE 0.8.0</a> to run)</li>
+</ul>
+</td><td width="50%" valign="top">
+This game can be downloaded and installed free of charge.<br>
+If you like <b>Eyes in the Night</b>, please consider supporting the author
+by buying the game for $.99, the price of a smartphone application.
+<form method=post action=https://api-3t.sandbox.paypal.com/nvp>
+<input type=hidden name=USER value=API_username>
+<input type=hidden name=PWD value=API_password>
+<input type=hidden name=SIGNATURE value=API_signature>
+<input type=hidden name=VERSION value=XX.0>
+<input type=hidden name=PAYMENTREQUEST_0_PAYMENTACTION value=Sale>
+<input name=PAYMENTREQUEST_0_AMT value=0.99 size=8>
+<input type=hidden name=RETURNURL value=http://www.rti-zone.org/eyes-in-the-night.php>
+<input type=hidden name=CANCELURL value=http://www.rti-zone.org/eyes-in-the-night.php>
+<input type=image
+src=https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif name=METHOD value=SetExpressCheckout>
+</form>
+
+<form method="post" action="https://www.paypal.com/cgi-bin/webscr">
+<input type="hidden" value="_s-xclick" name="cmd"/>
+<input border="0" type="image" alt="Make payments with PayPal - it's fast, free and secure!" name="submit" src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif"/>
+<img height="1" border="0" width="1" src="https://www.paypal.com/en_US/i/scr/pixel.gif" alt=""/>
+<input type="hidden" value="-----BEGIN PKCS7-----MIIHRwYJKoZIhvcNAQcEoIIHODCCBzQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAjmlgUAGEhcTJF/6aRxjw7PwKgFM7dlwTst4bPRSAbKJ34BFlLeNThLESc4stgZuQ0bKL8+o9Ktuux62iQQ55c+feT+xUBes0kkltkPlklmTJ4rJXqGviDUoyqDGIUFk/5Se6EcPw1U1u9B7JkM6eJUjlJalnTrMpwy4t1bPiA0TELMAkGBSsOAwIaBQAwgcQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIGSlM1QSHLFmAgaDy6an/KAzycRAmpEQXeU+ghSSHHHuyqFoddasgfE6byBzJGVqT4PbXYY7ZHTnfJIhRkB8d3NbabgF3y2blXqwV9GRDgTb8EM5IUvOBlbO/Dg72takfscrMKAYudr6x1vtN/jv1wtn2MwxmbNZGfGwSC+EIlquHUoOb8bMxGXCjTgPdlgKKEKAq1KpS65oLmzDchSOcQZYSxjdcT0jRDtvNoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMDgwMTA2MDcyOTM2WjAjBgkqhkiG9w0BCQQxFgQUu90k4jh+p+4ZXR1jdEbaNd55v60wDQYJKoZIhvcNAQEBBQAEgYBYuMKwvH9Piyp2Lnp9ZPU1qAePHn4nbbGzArUICYjSALWQJhlpKXrKghub7ngbDeJMdOp1NyAxaEeHskIcd1NhogiIiljfzUY0eGLPwdIZkDQsUFILhb3oVgljuYsDb3lJxB4Ps0+MUEld02pIeWXUkMSFkxR6482O3fMvGH14Sw==-----END PKCS7-----
+" name="encrypted"/>
+</form>
+
+</td>
+</tr></table>
+
+<br><br>
+By downloading this software you agree with the following terms of use:<br><br>
+
+<ol>
+  <li>This software is provided 'as-is', without any express or implied
+  warranty. In no event will the author be held liable for any damages
+  arising from the use of this software.</li>
+  <li>The origin of this software must not be misrepresented; you must not
+  claim that you wrote the original software.</li>
+  <li>You may not redistribute this software or a modified version of this software
+  without the permission of the author.</li>
+  <li>All copyrights to this software are exclusively owned by the author
+  - Eric Gaudet - except for the music which is licensed by Kevin MacLeod
+  (incompetech.com) under Creative Commons "Attribution 3.0"</li>
+</ol>
+<?php
+?>
+
